d70e0a64cbdbb8c2869c10da6a31e2e80fcf27f6
Karrie Dvorak <dvorak.karrie@gmail.com>
2020-02-20 09:30:00 -0600

    additional code factoring

--- js/main.js
+++ js/main.js
@@ -46,14 +46,11 @@
     let auxiliary = document.querySelector('#auxiliary')
     let destination = document.querySelector('#destination')
 
-    // Defined a variable for the user to restart the game.
+    // Defined a button variable for the user to select and restart the game.
     const restartButton = document.querySelector('.restartButton')
 
-    // Created a function to restart the game.
-    function restartGame() {
-        location.reload();
-    }
-    restartButton.addEventListener('click', restartGame)
+    // Created an event listener with a function to restart the game.
+    restartButton.addEventListener('click', () => location.reload())
 
     // Created functions and event listeners for the Instructions pop up window.
     function openPopUp() {
@@ -61,36 +58,36 @@
     }
     instructions.addEventListener('click', openPopUp)
 
-    function closePopUp() {
-        instructionPopUp.style.display = "none"
-    }
-    close.addEventListener('click', closePopUp)
+    // Created an event listener with function to close the instructions pop up window.
+    close.addEventListener('click', () => instructionPopUp.style.display = "none")
 
     // Created functions and event listeners for the Error pop up window.
     function openErrorPopUp(errorMessage) {
         popUpListError.innerText = errorMessage
         errorPopUp.style.display = 'block'
     }
+    
+    // Created an event listener and function to close the error message pop up window.
+    closeError.addEventListener('click', () => errorPopUp.style.display = "none")
 
-    function closeErrorPopUp() {
-        errorPopUp.style.display = "none"
-    }
-    closeError.addEventListener('click', closeErrorPopUp)
-
-    // Created a function to alert the user they won the game.
+    // Created a function to alert the user they won the game and play again by selecting the restart button.
     function win() {
         if (destination.childElementCount === parseInt(this.level)) {
             let message = document.querySelector('.message')
             message.style.display = "block"
             restartMessage.style.display = "block"
+
+            // Created function to stop the time counter.
             clearInterval(timeVariable)
+
+            // Event listeners created to block user from opening pop up windows and selecting disks.
             instructions.removeEventListener('click', openPopUp)
             dropdownButton.removeEventListener('click',getSelectedLevel)
             towerContainer.removeEventListener('click',moveDisk)
         }
     }
 
-    // Created a function to move disks to the selected tower.
+    // Created a function to move disks to the selected tower, increment a move counter and if user won after ensuring disks are in the correct order.
     function moveToTower(selectedDisk) {
         if (selectedTower.lastElementChild != null && selectedTower.lastElementChild.id > selectedDisk.id) {
             openErrorPopUp(errorLargerDisk)
@@ -142,7 +139,7 @@
         source.appendChild(diskArray[i])
     }
 
-    // Created a function to set up the level, number of disks and timer. 
+    // Created a function to set up the level, number of disks and begin counting time. 
     function getSelectedLevel() {
         level = document.querySelector('.dropdown').value
 
